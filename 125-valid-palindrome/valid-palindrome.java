963cc39ebaf1a0a8f68ea5816a3eb3b2553dd596
Gourab-Pal <109643343+Gourab-Pal@users.noreply.github.com>
2025-12-17 21:07:07 +0530

    Time: 14 ms (31.71%) | Memory: 46.3 MB (25.68%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s == null) return false;
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
+        int n = s.length();
+
+        int p1 = 0;
+        int p2 = n-1;
+        while(p1<p2) {
+            if(s.charAt(p1) != s.charAt(p2)) return false;
+            else {
+                p1++;
+                p2--;
+            }
+        }
+        return true;
+    }
+}

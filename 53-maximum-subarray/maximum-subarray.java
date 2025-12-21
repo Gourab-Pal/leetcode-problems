7e4acc910451247e65263003ea91764656ca2cba
Gourab-Pal <109643343+Gourab-Pal@users.noreply.github.com>
2025-12-21 21:20:33 +0530

    Time: 1 ms (99.78%) | Memory: 77.4 MB (14.24%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,9 +3,10 @@ class Solution {
         int n = nums.length;
         int maxSum = nums[0];
         int prefixSum = 0;
-
         for(int i=0; i<n; i++) {
-            if(prefixSum<0) prefixSum=0;
+            if(prefixSum<0) {
+                prefixSum = 0;
+            }
             prefixSum = prefixSum + nums[i];
             maxSum = Math.max(prefixSum, maxSum);
         }
